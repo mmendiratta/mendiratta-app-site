7936b7001dacb48e1f95d50059fe6e28273cd0d4
mmendiratta <mannimendiratta@yahoo.com>
2023-08-31 21:09:55 -0500

    eslint fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -53,11 +53,11 @@ class Contact extends Component {
 
   render() {
     if(this.props.data){
-      var name = this.props.data.name;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
+      // var name = this.props.data.name;
+      // var city = this.props.data.address.city;
+      // var state = this.props.data.address.state;
+      // var zip = this.props.data.address.zip;
+      // var phone= this.props.data.phone;
       var message = this.props.data.contactmessage;
     }
 
